ffdcc5f2beacefc8c41619bead57d4b53322de04
darkovasiljkov <vasiljkovdarko@gmail.com>
2025-09-14 19:59:28 +0200

    Go Back arrow implementation  and LeaDMagnetEditorNavBar Changes

--- convertly/app/(dashboard)/lead-magnet-editor/[[...leadMagnetId]]/components/LeadMagnetEditorNavBar.tsx
+++ convertly/app/(dashboard)/lead-magnet-editor/[[...leadMagnetId]]/components/LeadMagnetEditorNavBar.tsx
@@ -1,9 +1,28 @@
 import React, { use } from "react";
-
-
+import { useRouter } from "next/navigation";
+import { useLeadMagnetEditorContext } from "@/context/LeadMagnetEditorContext";
+import { BsArrowLeft } from "react-icons/bs";
 function LeadMagnetEditorNavBar ()
 {
     const {edittedLeadMagnet} = useLeadMagnetEditorContext();
+    const router = useRouter();
 
-
+return (
+    <div className="flex w-full flex-row items-center justify-between border-b-[1px] border-solid border-gray-200 bg-white p-3 text-gray-600">
+        <div className="flex flex-row items-center">
+        <BsArrowLeft
+        size={20}
+        className="pr-3 w-fit cursor-pointer"
+        onClick={() => void router.push("/lead-magnets")}
+        />
+         {/* TODO: Inpot / Name */}
+          {/* TODO: Edit / Save */}
+        </div>
+        <div className="flex flex-row items-center gap-x-4">
+             {/* TODO: Delete with state */}
+         {/* TODO: Unpublish and view Final */}
+          {/* TODO: ESave Publsih*/}
+        </div>
+    </div>
+);
 }
